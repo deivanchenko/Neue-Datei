3699a676c9511a685b240254e1697e9674718109
Denys <djonja@gmail.com>
2016-11-28 14:44:49 +0100

    neue js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 //Nichts Neues
-function setup() {
+var xoff = 0.0;
 var col = {
-  r: 2,
-  g: 33,
-  b: 222
+  
+  r: 155,
+  g: 40,
+  b: 103
 }
-}
+
 function draw() {
-randomSeed(99);
-for (var i=0; i < 100; i++) {
-  var r = random(0,100,10);
-  col.r = random(0, 2);
-  col.g = random(0, 33);
-  col.b = random(0, 222);
-  strokeWeight(4);
-  stroke(col.r,col.g,col.b);
-  noFill();
-  arc(i, 0,100, 160,i,50);
-}
+  background(214);
+  col.r = random(100, 155);
+  col.g = random(0, 40);
+  col.b = random(0, 103);
+  xoff = xoff + .01;
+  var n = noise(xoff) * width;
+  var x = randomGaussian(50,50);
+  fill(col.r,col.g,col.b);
+  ellipse(x, n, n, n);
+  ellipse(n, x, n, n);
+  ellipse(n, n, x, n);
+  ellipse(n, n, n, x);
 }
