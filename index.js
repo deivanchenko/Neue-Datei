bb28ea564c67c946632069de2b05d7f90ff5f639
Denys <djonja@gmail.com>
2016-11-28 14:57:37 +0100

    kleiner

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-//Nichts Neues
 var xoff = 0.0;
 var col = {
-  
+
   r: 155,
   g: 40,
   b: 103
 }
 
 function draw() {
-<<<<<<< HEAD
-	createCanvas(400,200);
-randomSeed(99);
-for (var i=0; i < 100; i++) {
-  var r = random(0,100,10);
-  col.r = random(0, 2);
-  col.g = random(0, 33);
-  col.b = random(0, 222);
-  strokeWeight(4);
-  stroke(col.r,col.g,col.b);
-  noFill();
-  arc(i, 0,100, 160,i,50);
-}
-=======
-  createCanvas(200,200);
+  createCanvas(100,100);
   background(214);
   col.r = random(100, 155);
   col.g = random(0, 40);
@@ -35,5 +20,4 @@ for (var i=0; i < 100; i++) {
   ellipse(n, x, n, n);
   ellipse(n, n, x, n);
   ellipse(n, n, n, x);
->>>>>>> origin/master
 }
